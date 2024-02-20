c421a6214d8b666ca236317bb1946fbdd56e562d
OstapMaksymiv <ostaperad@gmail.com>
2024-02-20 15:18:20 +0100

    feat: second try to fix text fulling function and add another pre-fix

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -15,11 +15,11 @@ mobilenav();
 darkMode();
 // const cursor = document.querySelector(".cursor");
 // const cursor_follow = document.querySelector(".cursor-follow");
-// const scrollUp = () => {
-//     const scrollUp = document.getElementById('scroll-up');
-//     window.scrollY >= 450 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
-// }
-// window.addEventListener('scroll', scrollUp);
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+    window.scrollY >= 450 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
 
 // window.addEventListener("mousemove", (e) => {
 //     gsap.to(".cursor",{x: e.clientX , y: e.clientY})
@@ -59,22 +59,22 @@ function addAnimation() {
     });
 }
 
-const target = document.getElementById('header');
-let prevScrollPos = window.scrollY;
-function handleScroll() {
-    const currentScrollPos = window.scrollY;
-    if (prevScrollPos > currentScrollPos) {
-        target.classList.remove('header__scroll-down');
-        target.classList.add('header__scroll-up');
-        target.style.position = "sticky"
-    } else {
-        target.classList.remove('header__scroll-up');
-        target.classList.add('header__scroll-down');
-        // target.style.position = "relative"
-    }
-    prevScrollPos = currentScrollPos;
-}
-window.addEventListener('scroll', handleScroll);
+// const target = document.getElementById('header');
+// let prevScrollPos = window.scrollY;
+// function handleScroll() {
+//     const currentScrollPos = window.scrollY;
+//     if (prevScrollPos > currentScrollPos) {
+//         target.classList.remove('header__scroll-down');
+//         target.classList.add('header__scroll-up');
+//         target.style.position = "sticky"
+//     } else {
+//         target.classList.remove('header__scroll-up');
+//         target.classList.add('header__scroll-down');
+//         // target.style.position = "relative"
+//     }
+//     prevScrollPos = currentScrollPos;
+// }
+// window.addEventListener('scroll', handleScroll);
 // const blurHeader = () => {
 //     const header = document.querySelector('.header');
 //     window.scrollY >= 50 ? header.classList.add('blur-header') 
