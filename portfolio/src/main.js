95b8fb52793baedb76c83b640b1552081631bc07
OstapMaksymiv <ostaperad@gmail.com>
2024-02-23 00:04:35 +0100

    refactor: clean the code

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -3,7 +3,6 @@ import '../styles/style.css';
 import '../styles/componants/header.css';
 import '../styles/componants/hero.css';
 import '../styles/componants/about.css';
-import '../styles/componants/featured.css';
 import '../styles/componants/work.css';
 import '../styles/componants/contact.css';
 import '../styles/componants/footer.css';
@@ -13,35 +12,11 @@ import mobilenav  from './utils/mobile-nav';
 import darkMode  from './utils/dark-theme';
 mobilenav();
 darkMode();
-// const cursor = document.querySelector(".cursor");
-// const cursor_follow = document.querySelector(".cursor-follow");
 const scrollUp = () => {
     const scrollUp = document.getElementById('scroll-up');
     window.scrollY >= 450 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll');
 }
 window.addEventListener('scroll', scrollUp);
-
-// window.addEventListener("mousemove", (e) => {
-//     gsap.to(".cursor",{x: e.clientX , y: e.clientY})
-//     gsap.to(".cursor-follow",{x: e.clientX , y: e.clientY, duration: 1.5, ease: "power3.out" })
-// })
-
-// document.addEventListener('mouseleave', function() {
-//     cursor.style.display = 'none';
-// });
-
-// document.addEventListener('mouseenter', function() {
-//     cursor.style.display = 'block';
-// });
-
-// document.addEventListener('mouseleave', function() {
-//     cursor_follow.style.display = 'none';
-// });
-
-// document.addEventListener('mouseenter', function() {
-//     cursor_follow.style.display = 'block';
-// });
-
 const scrollers = document.querySelectorAll(".scroller");
 if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches){
     addAnimation();
@@ -58,32 +33,6 @@ function addAnimation() {
         })
     });
 }
-
-// const target = document.getElementById('header');
-// let prevScrollPos = window.scrollY;
-// function handleScroll() {
-//     const currentScrollPos = window.scrollY;
-//     if (prevScrollPos > currentScrollPos) {
-//         target.classList.remove('header__scroll-down');
-//         target.classList.add('header__scroll-up');
-//         target.style.position = "sticky"
-//     } else {
-//         target.classList.remove('header__scroll-up');
-//         target.classList.add('header__scroll-down');
-//         // target.style.position = "relative"
-//     }
-//     prevScrollPos = currentScrollPos;
-// }
-// window.addEventListener('scroll', handleScroll);
-// const blurHeader = () => {
-//     const header = document.querySelector('.header');
-//     window.scrollY >= 50 ? header.classList.add('blur-header') 
-//                         : header.classList.remove('blur-header');
-//   }
-  
-// window.addEventListener('scroll', blurHeader);
-
-
 const sr = ScrollReveal({
     origin: 'top',
     distance: '40px',
@@ -91,11 +40,8 @@ const sr = ScrollReveal({
     scale: 1.1,
     duration: 2500,
     delay: 300,
-    // reset: true,
 })
 sr.reveal(`.hero__subtitle`)
-// sr.reveal(`.hero`,{rotate: {y: 3}} )
-
 sr.reveal(`.hero__img`,{scale:1,rotate: {y: 5}} )
 sr.reveal(`.header__img`,{scale:1, distance: '80px'} )
 sr.reveal(`.header__bars`,{scale:1, distance: '70px', delay:450} )
@@ -118,13 +64,5 @@ sr.reveal(`.contact__title`,{scale:0.9, } )
 sr.reveal(`.contact__description`,{scale:1, delay:350} )
 sr.reveal(`.footer__title`,{scale:1, origin: 'bottom',distance: '40px',opacity: 0} )
 sr.reveal(`.contact__btn`,{scale:0.8, distance:0} )
-// sr.reveal(`.header__sun`,{scale:1, distance: '100px', delay: 550, } )
-// sr.reveal(`.header__resume`,{scale:1, distance: '100px', delay: 550, } )
-// sr.reveal(`.header__link:nth-child(2)`,{rotate: {z: 15}, distance: 0 , delay: 600} )
-// sr.reveal(`.header__link:nth-child(3)`,{rotate: {z: -30}, distance: 0 , delay: 900} )
 
-// sr.reveal(`.favorite__card img`,{interval:100 ,rotate: {z: 15}, distance: 0 } )
-// sr.reveal(`.footer__container`,{scale: 1 } )
-// sr.reveal(`.footer__img-1`,{rotate: {z: 15}})
-// sr.reveal(`.footer__img-2`,{rotate: {z: 15}})
 
--- portfolio/src/utils/mobile-nav.js
+++ portfolio/src/utils/mobile-nav.js
@@ -2,21 +2,8 @@ const mobilenav = () => {
     const barss = document.querySelector('.header__bars');
     const mbnav = document .querySelector('.mobile-nav')
     const linkss = document.querySelectorAll('.mobile-nav__link')
-    const active = document.querySelector('.mobile-nav-active')
     const target = document.getElementById('header');
     const header__links = document.querySelectorAll('.header__link')
-    const highter = document.querySelector(".highter_body");
-    // if(barss){
-    //     barss.addEventListener('click', () => {
-    //         mbnav.classList.toggle("mobile-nav-active");
-    //     })
-    // }
-    // if(navClose){
-    //     navClose.addEventListener('click', () => {
-    //         navMenu.classList.remove('show-menu');
-    //     })
-    // }
-    
     let ismobileopen = false;
     let initialWindowWidth = window.innerWidth;
     barss.addEventListener("click", () => {
@@ -51,7 +38,6 @@ const mobilenav = () => {
                 newSVG.style.display = "none";
             })
         })
-        
         window.addEventListener('resize', function () {
             if (window.innerWidth > initialWindowWidth && window.innerWidth > 768 && initialWindowWidth <= 768) {
                 ismobileopen = false;
@@ -75,62 +61,6 @@ const mobilenav = () => {
                 elderSVG.style.display = "block";
                 newSVG.style.display = "none";
             }
-          });
-    // barss.addEventListener('click', ()=> {
-    //         ismobileopen = true;
-    //         mbnav.style.right = '0%' ;
-    //         document.body.style.overflowY= 'hidden' ;
-    // })
-    // cross.addEventListener('click', () => {
-    //     mbnav.style.right = '-100%' ;
-    //     document.body.style.overflowY= '' ;
-    // })
-    // linkss.forEach(element => {
-    //     element.addEventListener('click', ()=> {
-    //         mbnav.style.right = '-100%' ;
-    //         document.body.style.overflowY= '' ;
-    //     })
-    // })
-
-    // setInterval(checkMobileOpen, 100);
-    // barss.addEventListener('click', toggleBurger);
-    
-    // function toggleBurger() {
-    //     window.scrollTo(0, 0);
-    //     body.classList.toggle('lock');
-    //     mbnav.classList.toggle('mobile-nav-active');
-    
-    // }
-    
+          });    
 }
-
-
-
-
-
-
-
-
-// /*=============== SHOW MENU ===============*/
-// const navMenu = document.getElementById('nav-menu'),
-//         navToggle = document.getElementById('nav-toggle'),
-//         navClose = document.getElementById('nav-close');
-// if(navToggle) {
-//     navToggle.addEventListener('click', () => {
-//         navMenu.classList.add('show-menu');
-//     });
-// };
-// if(navClose) {
-//     navClose.addEventListener('click' , () => {
-//         navMenu.classList.remove('show-menu');
-//     });
-// };
-// /*=============== REMOVE MENU MOBILE ===============*/
-// const navLink = document.querySelectorAll('.nav__link');
-// const linkAction = () => {
-//     const navMenu = document.getElementById('nav-menu')
-//     navMenu.classList.remove('show-menu')
-// } 
-// navLink.forEach(e => e.addEventListener('click', linkAction));
-
 export default mobilenav;
